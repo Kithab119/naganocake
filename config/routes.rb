93f4d627ef5ebc6a95e258d72eab857f9f3b7e37
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-18 07:36:35 +0000

    [route-OK]

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,26 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :public do
-    get "/" => "homes#top"
+  scope module: :public do #URLは変わらずに、コントローラを呼び出せる
+    root to: "homes#top" #"文字列型"、'文字列型、変数'
     get "/about" => "homes#about"
     resources :items, only: [:index, :show]
     resources :registrations, only: [:new, :create]
     resources :sessions, only: [:new, :create, :destroy]
-    resource :customers, only: [:show, :edit, :update,]
     get "/customers/out" => "customers#out"
     patch "/customers/out_check" => "customers#out_check"
-    resources :cart_items, only: [:index, :update, :destroy, :create]
-    delete "/cart_items/destroy_all" => "cart_items#destroy_all"
-    resources :orders, only: [:new, :create, :index, :show]
+    resource :customers, only: [:show, :edit, :update,]
+    delete "/cart_items/destroy_all" => "cart_items#destroy_all" #destroyのURL => cart_items/destroy_all
+    resources :cart_items, only: [:index, :update, :destroy, :create] #destroyのURL => cart_items/:id、上記のURLと重複する為、優先度を下にする。
     post "/orders/order_check" => "orders#order_check"
     get "/orders/finish" => "orders#finish"
+    resources :orders, only: [:new, :create, :index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
-  namespace :admin do
+  namespace :admin do #URLに権限を付けて、コントローラを呼び出せる
+    root to: "homes#top"
     resources :sessions, only: [:new, :create, :destroy]
-    get "/" => "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
