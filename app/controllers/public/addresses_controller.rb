a5029c2519e06dcfa6fb70505ab0a90910d727fc
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-27 15:54:05 +0000

    [23.03.27]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -8,7 +8,7 @@ class Public::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-     #binding.pry
+     #binding.pry(アクションをここで止める)  Gemfileにgem 'pry-byebug'
     @address.save
     redirect_to addresses_path
   end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,15 @@
 class Public::OrdersController < ApplicationController
+
+  def new
+    @order = Order.
+  end
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
 end
