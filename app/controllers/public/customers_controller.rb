e84d7aec6f4662c4d76e05afb4ebc39ad6754ed4
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-27 12:47:53 +0000

    [address]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,7 +18,10 @@ class Public::CustomersController < ApplicationController
   end
 
   def out_check
-
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
 
