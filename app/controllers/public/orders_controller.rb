f2830342cbbb2466dc97c748834aac99c3c85a74
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-30 12:32:23 +0000

    [order new]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def order_check
+    @cart_items = current_customer.cart_items
+    @sum = 0
+    @order = Order.new(order_params)
+    
+    if select_address == 0
+      @order.shipping_postal_code = current_customer.postal_code
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.first_name + current_customer.last_name
+    elsif select_address == 1
+      address_
+    else select_address == 2
+      新しく登録したアドレス
+    end
   end
 
   def index
@@ -16,7 +29,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new()
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to orders_finish_path
   end
+
+  def finish
+  end
+
+
+  private
+    def order_params
+      params.require(:order).permit(:shipping_postal_code, :shipping_address, :shipping_name, :postage, :claim, :payment, :is_receive)
+    end
 
 end
