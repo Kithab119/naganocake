04c82bd6e24933e47cf718639309a99b529a7e2d
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-30 14:59:36 +0000

    [bootstrap]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,15 +8,17 @@ class Public::OrdersController < ApplicationController
     @cart_items = current_customer.cart_items
     @sum = 0
     @order = Order.new(order_params)
-    
+
     if select_address == 0
       @order.shipping_postal_code = current_customer.postal_code
       @order.shipping_address = current_customer.address
       @order.shipping_name = current_customer.first_name + current_customer.last_name
     elsif select_address == 1
-      address_
+      address
     else select_address == 2
-      新しく登録したアドレス
+      @order.shipping_postal_code
+      @order.shipping_address
+      @order.shipping_name
     end
   end
 
