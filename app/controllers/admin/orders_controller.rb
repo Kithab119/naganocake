7efc1b4b818df624c8992d49eb4b91d5253e4fd7
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-04-05 16:04:18 +0000

    [23.04.05]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,17 +2,20 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
+    @sum = 0
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
   end
 
 
   private
     def order_params
-      params.require(:order, :order_detail).parmit(:is_receive, :is_create)
+      params.require(:order).permit(:is_receive)
     end
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,4 +4,6 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :item
 
+  enum is_create: { cannot_manufacture: 0, waiting_manufacture: 1, manufacture: 2, complete_manufacture: 3 }
+
 end
