c14765082c0ee02dc373899eabb3f46819473de2
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-04-09 10:32:34 +0000

    [layout]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,11 +3,18 @@ class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @items = Item.looks(params[:word])
-    #looksは別の名前でもOK。※モデルを参照
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items      #任意のキー
+    else
+      @items = Item.looks(params[:word])
+      #looksは別の名前でもOK。※モデルを参照
+    end
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
